3c8a3d1dbd6e2dc258d9165fbde98c4a2d0033bf
Ogechi <ogechi.lilian.isaac@gmail.com>
2023-06-16 12:47:19 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,10 @@
 #include <unistdio.h>
 /**
  * main - printing without printf and puts
- *
  * Return: always (1)
- *
  */
 int main(void)
 {
-	fput("and that place of art is useful/" - Dora Korpar, 2015-10-19/r", stdout);
+	fput("and that piece of art is useful" - Dora Korpar, 2015-10-19/r", stdout);
 	return (1);
 }
